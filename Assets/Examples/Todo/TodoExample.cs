49fefbecdaf15025b508aa421aabd1f18310b0d8
Benjamin Buß <benjamin.buss@gmx.de>
2024-05-09 20:57:27 +0200

    Moved stylesheets out of ui doc

--- Assets/Examples/Todo/TodoExample.cs
+++ Assets/Examples/Todo/TodoExample.cs
@@ -9,11 +9,16 @@ namespace Examples.Todo
     {
         [SerializeField] 
         private UIDocument _uiDocument;
+        [SerializeField]
+        private StyleSheet[] _styles;
         
         private UIRenderer _renderer;
         
         private void Start()
         {
+            foreach (var style in _styles)
+                _uiDocument.rootVisualElement.styleSheets.Add(style);
+
             _renderer = new UIRenderer();
             _renderer.Render(ctx => ctx.CreateComponent<App>(), _uiDocument.rootVisualElement);
         }
