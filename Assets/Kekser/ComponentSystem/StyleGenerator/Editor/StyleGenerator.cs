da6f5cb494ef10cdaf60eb6b49a9ab6b68bb3323
Benjamin Buß <benjamin.buss@gmx.de>
2024-05-11 18:42:44 +0200

    added text shadow prop

--- Assets/Kekser/ComponentSystem/StyleGenerator/Editor/StyleGenerator.cs
+++ Assets/Kekser/ComponentSystem/StyleGenerator/Editor/StyleGenerator.cs
@@ -242,7 +242,8 @@ namespace Kekser.ComponentSystem.StyleGenerator
             new StyleRule("overflow-hidden", StyleRule.Unit.None, _ => "overflow: hidden"),
             
             new StyleRule("text-align", StyleRule.Unit.String, value => $"-unity-text-align: {value}"),
-            
+            new StyleRule("test-shadow", StyleRule.Unit.String, value => $"text-shadow: {value}"),
+                
             new StyleRule("text-ellipsis", StyleRule.Unit.None, _ => "text-overflow: ellipsis"),
             new StyleRule("text-wrap", StyleRule.Unit.None, _ => "white-space: wrap"),
             new StyleRule("text-nowrap", StyleRule.Unit.None, _ => "white-space: nowrap"),
